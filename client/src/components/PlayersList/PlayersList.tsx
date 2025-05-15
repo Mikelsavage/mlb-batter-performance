60f7d2e0eb4b518e88921580de964c7a26a7a32c
Nick Mikelsavage <n.mikelsavage@gmail.com>
2025-05-15 12:43:45 -0400

    sort by asc or desc

--- client/src/components/PlayersList/PlayersList.tsx
+++ client/src/components/PlayersList/PlayersList.tsx
@@ -1,13 +1,17 @@
 import isEmpty from "lodash/isEmpty";
+import orderBy from "lodash/orderBy";
 import { PlayerCard } from "../PlayerCard/PlayerCard";
 import styles from "./PlayersList.module.scss";
 import { useTranslation } from "react-i18next";
 import { useGetPlayersWithToken } from "../../services/players";
 import { useMemo, useState } from "react";
 
+type SortOrderType = "asc" | "desc";
+
 export const PlayersList = () => {
     const { players = [], isLoading, error } = useGetPlayersWithToken();
     const [searchValue, setSearchValue] = useState("");
+    const [sortOrder, setSortOrder] = useState<SortOrderType>("asc");
 
     const { t } = useTranslation();
 
@@ -35,20 +39,67 @@ export const PlayersList = () => {
 
     return (
         <>
-            <div className={styles.searchContainer}>
-                <form
-                    className={styles.searchForm}
-                    onSubmit={(e) => e.preventDefault()}
-                >
-                    <label htmlFor="search">{t("players.search.label")}</label>
-                    <input
-                        className={styles.searchInput}
-                        id="search-value"
-                        onChange={handleSearchInputChange}
-                        type="text"
-                        placeholder={t("players.search.placeholder")}
-                    />
-                </form>
+            <div className={styles.filters}>
+                <div className={styles.searchContainer}>
+                    <form
+                        className={styles.searchForm}
+                        onSubmit={(e) => e.preventDefault()}
+                    >
+                        <label htmlFor="search">
+                            {t("players.search.label")}
+                        </label>
+                        <input
+                            className={styles.searchInput}
+                            id="search-value"
+                            onChange={handleSearchInputChange}
+                            type="text"
+                            placeholder={t("players.search.placeholder")}
+                        />
+                    </form>
+                </div>
+
+                <div className={styles.sortContainer}>
+                    <label htmlFor="sort">{t("players.sort.label")}</label>
+
+                    <div className={styles.buttonGroup}>
+                        <label
+                            className={styles.sortButtonContainer}
+                            htmlFor="sort-asc"
+                        >
+                            <input
+                                type="radio"
+                                id="sort-asc"
+                                name="sort"
+                                value="asc"
+                                checked={sortOrder === "asc"}
+                                onChange={() => setSortOrder("asc")}
+                            />
+
+                            <span className={styles.sortButton}>
+                                {t("players.sort.asc")}
+                            </span>
+                        </label>
+
+                        <label
+                            className={styles.sortButtonContainer}
+                            htmlFor="sort-desc"
+                        >
+                            <input
+                                className={styles.sortButton}
+                                type="radio"
+                                id="sort-desc"
+                                name="sort"
+                                value="desc"
+                                checked={sortOrder === "desc"}
+                                onChange={() => setSortOrder("desc")}
+                            />
+
+                            <span className={styles.sortButton}>
+                                {t("players.sort.desc")}
+                            </span>
+                        </label>
+                    </div>
+                </div>
             </div>
 
             {isEmpty(filteredPlayers) ? (
@@ -63,11 +114,13 @@ export const PlayersList = () => {
                 )
             ) : (
                 <ul className={styles.list}>
-                    {filteredPlayers?.map((player) => (
-                        <li key={player.playerId}>
-                            <PlayerCard player={player} />
-                        </li>
-                    ))}
+                    {orderBy(filteredPlayers, "playerFullName", sortOrder)?.map(
+                        (player) => (
+                            <li key={player.playerId}>
+                                <PlayerCard player={player} />
+                            </li>
+                        )
+                    )}
                 </ul>
             )}
         </>
